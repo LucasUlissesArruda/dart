3342f71aafb6594723264ca0b412b8470ba56918
Lucas Arruda <168601194+LucasUlissesArruda@users.noreply.github.com>
2025-09-01 17:01:37 -0300

    trabalho Dart

---
+++ trabalho dart/trabalho.dart
@@ -0,0 +1,40 @@
+void main() {
+  int jovens = 2;
+  int adultos = 0;
+  int mes = 0;
+
+  for (mes = 1; mes <= 12; mes++) {
+    int novosAdultos = jovens;
+    adultos += novosAdultos;
+
+    int novosJovens = adultos;
+
+    jovens = novosJovens;
+
+    int total = jovens + adultos;
+    print('Fim do Mês $mes: População de coelhos: $total, Jovens: $jovens, Adultos: $adultos');
+  }
+
+  print('\nIntrodução do predador: redução de 75% da população a cada mês.\n');
+
+  for (mes = 13; mes <= 18; mes++) {
+    int novosAdultos = jovens;
+    adultos += novosAdultos;
+
+    int novosJovens = adultos;
+
+    jovens = novosJovens;
+
+    int total = jovens + adultos;
+    int totalAposPredacao = (total * 0.25).floor();
+
+    double proporcaoJovens = jovens / total;
+    double proporcaoAdultos = adultos / total;
+
+    jovens = (totalAposPredacao * proporcaoJovens).floor();
+    adultos = (totalAposPredacao * proporcaoAdultos).floor();
+
+    total = jovens + adultos;
+    print('Fim do Mês $mes: População de coelhos: $total, Jovens: $jovens, Adultos: $adultos');
+  }
+}
